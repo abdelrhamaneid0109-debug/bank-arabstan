fe52008e6975bc80bd083be71cfcdd19db73b3f4
abdelrhamaneid0109-debug <abdelrhamaneid0109@gmail.com>
2026-04-13 13:45:05 +0300

    Update confirm.js

--- commands/confirm.js
+++ commands/confirm.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { Pool } = require("pg");
-const { getUser, updateUser, createUser } = require("../db/db");
+const { getUser, updateUser, createUser } = require("../db/db.js");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
