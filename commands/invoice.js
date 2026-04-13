85a37a252daeb781c20bcecfe1d9a0ff2b31f1ef
abdelrhamaneid0109-debug <abdelrhamaneid0109@gmail.com>
2026-04-13 13:20:37 +0300

    Add role check for merchant commands

---
+++ commands/invoice.js
@@ -0,0 +1,16 @@
+const merchantRoleId = "PUT_ROLE_ID_HERE";
+
+if (!interaction.member.roles.cache.has(merchantRoleId)) {
+  return interaction.reply({
+    content: "❌ الأمر ده للتجار فقط",
+    ephemeral: true
+  });
+}
+const merchantRoleId = "PUT_ROLE_ID_HERE";
+
+if (!interaction.member.roles.cache.has(merchantRoleId)) {
+  return interaction.reply({
+    content: "❌ الأمر ده للتجار فقط",
+    ephemeral: true
+  });
+}
